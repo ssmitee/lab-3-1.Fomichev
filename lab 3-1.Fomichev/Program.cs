50a6d16736a043609983e6caeca83eb0855435af
ssmitee <144239782+ssmitee@users.noreply.github.com>
2023-12-23 20:50:26 +0200

    Добавьте файлы проекта.

---
+++ lab 3-1.Fomichev/Program.cs
@@ -0,0 +1,12 @@
+﻿int N = 5;
+double A = 0.0;
+double B = 10.0;
+
+double H = (B - A) / N;
+double currentPoint = A;
+
+for (int i = 0; i <= N; i++)
+{
+    Console.WriteLine("Точка " + (i + 1) + ": " + currentPoint);
+    currentPoint += H;
+}
